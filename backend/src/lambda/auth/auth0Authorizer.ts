2913f9a9dd90dae70c613a18dcaed4eb514eb3df
Massa Koné <36091977+MassaKone@users.noreply.github.com>
2022-10-21 18:12:01 -0400

    Beta build - Ready to test deployment

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -11,7 +11,7 @@ const logger = createLogger('auth')
 
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = 'https://nooreldeensalah.us.auth0.com/.well-known/jwks.json'
+const jwksUrl = 'https://massakone.us.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
